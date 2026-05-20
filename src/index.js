f0f7981555b7d603bfe7e954b3dd07e8e2fc2200
Webevora <info@webevora.com>
2026-05-20 17:58:55 +0530

    final github pages fix

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import { BrowserRouter } from 'react-router-dom';
 import './css/style.css';
 import App from './App';
 
+// redirect handling for GitHub Pages 404.html workaround
+const redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+
+if (redirect && redirect !== location.href) {
+  history.replaceState(null, null, redirect);
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
